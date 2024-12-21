d751729538982c22b2ac740e4950291e5a880b53
Ogundipe Naomi <ogundipenaomi@gmail.com>
2024-12-21 22:07:22 +0100

    merged changes with edits

---
+++ client/public/src/javascript/fulfilment.js
@@ -0,0 +1,20 @@
+const inquiryForm = document.getElementById("inquiryForm");
+const signupForm = document.getElementById("signupForm");
+const submit = document.getElementById("inquirysubmit");
+
+// inquiryForm.addEventListener('submit',function(e){
+//     e.preventDefault();
+//     if (inquiryForm.checkValidity()){
+//         alert('Form has been succesfully created')
+//         inquiryForm.style.display='none';
+//         signupForm.style.display='block';
+//     }
+// })
+
+// signupForm.addEventListener('submit',function(e){
+//     e.preventDefault();
+//     alert('Sign up Successful!')
+// })
+inquiryForm.addEventListener('submit',function(){
+    alert('Form has been succesfully created')
+})
